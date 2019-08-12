91d82c7bb7e89b7b70ef0f3badb9e1d8ad318c39
Netin <Netin@DESKTOP-3612JVG>
2019-08-12 15:25:25 +1200

    Add project files.

---
+++ RussianRouletteGame/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RussianRouletteGame
+{
+    public partial class Startup : Form
+
+    {
+        public Startup()
+        {
+            InitializeComponent();
+        }
+
+        private void Startup_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            Game f1 = new Game();
+            f1.Show();
+            this.Visible = false;
+            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Kill process
+            Application.Exit();
+        } 
+        
+
+    }
+}
